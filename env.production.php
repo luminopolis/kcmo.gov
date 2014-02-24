0c00f23e3a98a27732d0ce41e5ea786826f0adc2
petergibbons <bristond@gmail.com>
2014-02-24 12:55:50 -0600

    Update env.production.php
    Remove project specific paths / pw

--- env.production.php
+++ env.production.php
@@ -2,16 +2,15 @@
 
 // @see https://github.com/x-team/config-driven-wp/
 return array(
-	// wp_engine prod
 	'DB_HOST' => '127.0.0.1',
 	'DB_HOST_SLAVE' => 'localhost',
-	'DB_NAME' => 'wp_qdi478t',
-	'DB_PASSWORD' => 'WLlmIQspex6n8lAUAwLf',
-	'DB_USER' => 'qdi478t',
+	'DB_NAME' => 'DB NAME HERE',
+	'DB_PASSWORD' => 'DB PASS HERE',
+	'DB_USER' => 'DB USER HERE',
 	'DB_CHARSET' => 'utf8',
 	'DB_COLLATE' => 'utf8_unicode_ci',
 	
-	'table_prefix' => 'kcmosand_',	//'wp_'
+	'table_prefix' => 'table_prefix_',	//'wp_'
 
 	'WP_DEBUG' => TRUE,
 	'WPLANG' => '',
@@ -29,31 +28,7 @@ return array(
 	'NONCE_SALT'       => '17ymMLf8ka0x5rme3s2Hdgk97Xv8G7Z78QuWD73831IQEXO3mH',
 	'WP_CACHE' => FALSE,
 	'WP_AUTO_UPDATE_CORE' => FALSE,
-	'PWP_NAME' => 'qdi478t',
 
-	'FS_METHOD' => 'direct',
-	'FS_CHMOD_DIR' => 0775,
-	'FS_CHMOD_FILE' => 0664,
-	'PWP_ROOT_DIR' => '/nas/wp',
-	'WPE_APIKEY' => '77cb26c233cd28562c1834a5f8f3315a7011056d',
-	'WPE_FOOTER_HTML' => '',
-	'WPE_CLUSTER_ID' => '1598',
-	'WPE_CLUSTER_TYPE' => 'pod',
-	'WPE_ISP' => TRUE,
-	'WPE_BPOD' => FALSE,
-	'WPE_RO_FILESYSTEM' => FALSE,
-	'WPE_LARGEFS_BUCKET' => 'largefs.wpengine',
-	'WPE_CDN_DISABLE_ALLOWED' => TRUE,
-	'DISALLOW_FILE_EDIT' => FALSE,
-	'DISALLOW_FILE_MODS' => FALSE,
-	'DISABLE_WP_CRON' => FALSE,
-	'WPE_FORCE_SSL_LOGIN' => FALSE,
-	'FORCE_SSL_LOGIN' => FALSE,
-	'WPE_EXTERNAL_URL' => FALSE,
-	'WP_POST_REVISIONS' => FALSE,
-	'WPE_WHITELABEL' => 'wpengine',
-	'WP_TURN_OFF_ADMIN_BAR' => FALSE,
-	'WPE_BETA_TESTER' => FALSE,
 	//
 	
 	// Multisite constants
